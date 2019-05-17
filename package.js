a4d70437b641661df5b64ab9fe42a4763e5b9c39
Adam Wolski <wolas.abc@gmail.com>
2019-05-17 19:50:45 +0200

    update git url

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'wolas:sentry',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
   summary: 'A simple wrapper for sentry SDK. Allows to use sentry in both client and server in meteor apps.',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  git: 'https://github.com/wolasss/meteor-sentry',
   documentation: 'README.md'
 });
 
