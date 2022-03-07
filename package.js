a6dd8dbd00e9fc9eac630f9e3f61c4e1817365d9
Adam Wolski <wolas.abc@gmail.com>
2022-03-07 14:57:58 +0100

    chore: bump deps

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: 'wolas:sentry',
-  version: '1.0.3',
+  version: '1.0.6',
   summary: 'A simple wrapper for sentry SDK. Allows to use sentry in both client and server in meteor apps.',
   git: 'https://github.com/wolasss/meteor-sentry',
   documentation: 'README.md'
 });
 
 Npm.depends({
-  '@sentry/browser': '5.13.0',
-  '@sentry/node': '5.13.1'
+  '@sentry/browser': '6.18.1',
+  '@sentry/node': '6.18.1'
 });
 
 Package.onUse(function(api) {
