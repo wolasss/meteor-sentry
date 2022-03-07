51bd4e5e83dcebd827e97632f8924641e3a14d35
Adam Wolski <wolas.abc@gmail.com>
2022-03-07 15:13:33 +0100

    fix: fix issue while loading sentry/node in the browser

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'wolas:sentry',
-  version: '1.0.6',
+  version: '1.0.7',
   summary: 'A simple wrapper for sentry SDK. Allows to use sentry in both client and server in meteor apps.',
   git: 'https://github.com/wolasss/meteor-sentry',
   documentation: 'README.md'
@@ -16,5 +16,6 @@ Package.onUse(function(api) {
   api.use('ecmascript');
   api.addFiles('lib/client.js', 'client');
   api.addFiles('lib/server.js', 'server');
-  api.mainModule('lib/logger.js');
+  api.mainModule('lib/logger_client.js', 'client');
+  api.mainModule('lib/logger_server.js', 'server');
 });
