337056f8ec341d95b219d4fdd81e0c1528635fa6
Adam Wolski <wolas.abc@gmail.com>
2024-08-16 15:01:37 +0200

    chore: upgrade sentry to 8.26.0

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: 'wolas:sentry',
-  version: '1.0.7',
+  version: '1.0.8',
   summary: 'A simple wrapper for sentry SDK. Allows to use sentry in both client and server in meteor apps.',
   git: 'https://github.com/wolasss/meteor-sentry',
   documentation: 'README.md'
 });
 
 Npm.depends({
-  '@sentry/browser': '6.18.1',
-  '@sentry/node': '6.18.1'
+  '@sentry/browser': '8.26.0',
+  '@sentry/node': '8.26.0'
 });
 
 Package.onUse(function(api) {
